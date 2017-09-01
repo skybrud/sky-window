4382a4204fddeef96a0f087edd79dc7b7bfa71f9
Rudi Ørnhøj <rornhoj@gmail.com>
2017-09-01 13:25:01 +0200

    Fixed implementation issue on resize event

--- sky-window.js
+++ sky-window.js
@@ -1,10 +1,20 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/timer';
-import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/first';
+
+const _window = typeof window === 'undefined' ? {} : window;
 
 export default {
-	resize: Observable.fromEvent(window, 'resize').share().debounce(() => Observable.timer(100)),
-	scroll: Observable.fromEvent(window, 'scroll').share(),
+	on(eventName) {
+		return Observable.fromEvent(_window, eventName).share();
+	},
+	once(eventName) {
+		return Observable.fromEvent(_window, eventName).share().first();
+	},
+	load: Observable.fromEvent(_window, 'load').share().first(),
+	instantResize: Observable.fromEvent(_window, 'resize').share().debounceTime(40),
+	resize: Observable.fromEvent(_window, 'resize').share().debounceTime(200),
+	scroll: Observable.fromEvent(_window, 'scroll').share(),
 };
